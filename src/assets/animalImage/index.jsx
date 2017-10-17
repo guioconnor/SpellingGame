679f79e92b7c20541e590bdd5b6aea327573cdaf
Guilherme O'Connor <guilherme@fundapps.co>
2017-10-18 00:58:02 +0100

    Alternative images

--- src/assets/animalImage/index.jsx
+++ src/assets/animalImage/index.jsx
@@ -1,14 +1,20 @@
 import bear from './img/bear.svg';
+import beaver from './img/beaver.svg';
 import butterfly from './img/butterfly.svg';
 import cat from './img/cat.svg';
 import chameleon from './img/chameleon.svg';
+import chick from './img/chick.svg';
 import chicken from './img/chicken.svg';
 import chicken1 from './img/chicken1.svg';
+import cow from './img/cow.svg';
 import crab from './img/crab.svg';
+import deer from './img/deer.svg';
 import dog from './img/dog.svg';
+import donkey from './img/donkey.svg';
 import duck from './img/duck.svg';
 import elephant from './img/elephant.svg';
 import fish from './img/fish.svg';
+import fox from './img/fox.svg';
 import frog from './img/frog.svg';
 import giraffe from './img/giraffe.svg';
 import hedgehog from './img/hedgehog.svg';
@@ -21,9 +27,11 @@ import octopus from './img/octopus.svg';
 import ostrich from './img/ostrich.svg';
 import owl from './img/owl.svg';
 import owl1 from './img/owl1.svg';
+import panda from './img/panda.svg';
 import penguin from './img/penguin.svg';
 import pig from './img/pig.svg';
 import rabbit from './img/rabbit.svg';
+import racoon from './img/racoon.svg';
 import rooster from './img/rooster.svg';
 import shark from './img/shark.svg';
 import sheep from './img/sheep.svg';
@@ -31,11 +39,14 @@ import sheep2 from './img/sheep2.svg';
 import snail from './img/snail.svg';
 import squid from './img/squid.svg';
 import squirrel from './img/squirrel.svg';
+import tiger from './img/tiger.svg';
 import unknown01 from './img/unknown01.svg';
 import unknown02 from './img/unknown02.svg';
 import unknown03 from './img/unknown03.svg';
 import unknown04 from './img/unknown04.svg';
+import walrus from './img/walrus.svg';
 import wolf from './img/wolf.svg';
+import zebra from './img/zebra.svg';
 
 import bearSilhouette from './silhouettes/bear.svg';
 import butterflySilhouette from './silhouettes/butterfly.svg';
@@ -78,16 +89,22 @@ import wolfSilhouette from './silhouettes/wolf.svg';
 
 const images = {
   bear,
+  beaver,
   butterfly,
   cat,
   chameleon,
+  chick,
   chicken,
   chicken1,
+  cow,
   crab,
+  deer,
   dog,
+  donkey,
   duck,
   elephant,
   fish,
+  fox,
   frog,
   giraffe,
   hedgehog,
@@ -100,9 +117,11 @@ const images = {
   ostrich,
   owl,
   owl1,
+  panda,
   penguin,
   pig,
   rabbit,
+  racoon,
   rooster,
   shark,
   sheep,
@@ -110,11 +129,14 @@ const images = {
   snail,
   squid,
   squirrel,
+  tiger,
   unknown01,
   unknown02,
   unknown03,
   unknown04,
+  walrus,
   wolf,
+  zebra,
 }
 
 const silhouettes = {
--- src/data/animalsData.js
+++ src/data/animalsData.js
@@ -4,19 +4,29 @@ const animalsData = {
     name: 'bear',
     difficulty: 2,
   },
-  butterfly: {
-    animalId: 'butterfly',
-    name: 'butterfly',
+  beaver: {
+    animalId: 'beaver',
+    name: 'beaver',
     difficulty: 2,
   },
+  // butterfly: {
+  //   animalId: 'butterfly',
+  //   name: 'butterfly',
+  //   difficulty: 2,
+  // },
   cat: {
     animalId: 'cat',
     name: 'cat',
     difficulty: 1,
   },
-  chameleon: {
-    animalId: 'chameleon',
-    name: 'chameleon',
+  // chameleon: {
+  //   animalId: 'chameleon',
+  //   name: 'chameleon',
+  //   difficulty: 2,
+  // },
+  chick: {
+    animalId: 'chick',
+    name: 'chick',
     difficulty: 2,
   },
   chicken: {
@@ -24,19 +34,34 @@ const animalsData = {
     name: 'chicken',
     difficulty: 2,
   },
-  chicken1: {
-    animalId: 'chicken1',
-    name: 'chicken',
-    difficulty: 2,
+  // chicken1: {
+  //   animalId: 'chicken1',
+  //   name: 'chicken',
+  //   difficulty: 2,
+  // },
+  cow: {
+    animalId: 'cow',
+    name: 'cow',
+    difficulty: 1,
   },
-  crab: {
-    animalId: 'crab',
-    name: 'crab',
+  // crab: {
+  //   animalId: 'crab',
+  //   name: 'crab',
+  //   difficulty: 1,
+  // },
+  deer: {
+    animalId: 'deer',
+    name: 'deer',
     difficulty: 1,
   },
   dog: {
     animalId: 'dog',
     name: 'dog',
+    difficulty: 1,
+  },
+  donkey: {
+    animalId: 'donkey',
+    name: 'donkey',
     difficulty: 1,
   },
   duck: {
@@ -49,26 +74,31 @@ const animalsData = {
     name: 'elephant',
     difficulty: 2,
   },
-  fish: {
-    animalId: 'fish',
-    name: 'fish',
+  // fish: {
+  //   animalId: 'fish',
+  //   name: 'fish',
+  //   difficulty: 2,
+  // },
+  fox: {
+    animalId: 'fox',
+    name: 'fox',
     difficulty: 2,
   },
-  frog: {
-    animalId: 'frog',
-    name: 'frog',
-    difficulty: 1,
-  },
-  giraffe: {
-    animalId: 'giraffe',
-    name: 'giraffe',
-    difficulty: 2,
-  },
-  hippo: {
-    animalId: 'hippo',
-    name: 'hippo',
-    difficulty: 2,
-  },
+  // frog: {
+  //   animalId: 'frog',
+  //   name: 'frog',
+  //   difficulty: 1,
+  // },
+  // giraffe: {
+  //   animalId: 'giraffe',
+  //   name: 'giraffe',
+  //   difficulty: 2,
+  // },
+  // hippo: {
+  //   animalId: 'hippo',
+  //   name: 'hippo',
+  //   difficulty: 2,
+  // },
   lion: {
     animalId: 'lion',
     name: 'lion',
@@ -79,34 +109,39 @@ const animalsData = {
     name: 'monkey',
     difficulty: 2,
   },
-  mosquito: {
-    animalId: 'mosquito',
-    name: 'mosquito',
-    difficulty: 2,
-  },
-  mouse: {
-    animalId: 'mouse',
-    name: 'mouse',
-    difficulty: 2,
-  },
-  octopus: {
-    animalId: 'octopus',
-    name: 'octopus',
-    difficulty: 2,
-  },
-  ostrich: {
-    animalId: 'ostrich',
-    name: 'ostrich',
-    difficulty: 2,
-  },
+  // mosquito: {
+  //   animalId: 'mosquito',
+  //   name: 'mosquito',
+  //   difficulty: 2,
+  // },
+  // mouse: {
+  //   animalId: 'mouse',
+  //   name: 'mouse',
+  //   difficulty: 2,
+  // },
+  // octopus: {
+  //   animalId: 'octopus',
+  //   name: 'octopus',
+  //   difficulty: 2,
+  // },
+  // ostrich: {
+  //   animalId: 'ostrich',
+  //   name: 'ostrich',
+  //   difficulty: 2,
+  // },
   owl: {
     animalId: 'owl',
     name: 'owl',
     difficulty: 2,
   },
-  owl1: {
-    animalId: 'owl1',
-    name: 'owl1',
+  // owl1: {
+  //   animalId: 'owl1',
+  //   name: 'owl1',
+  //   difficulty: 2,
+  // },
+  panda: {
+    animalId: 'panda',
+    name: 'panda',
     difficulty: 2,
   },
   penguin: {
@@ -124,16 +159,21 @@ const animalsData = {
     name: 'rabbit',
     difficulty: 2,
   },
-  rooster: {
-    animalId: 'rooster',
-    name: 'rooster',
+  racoon: {
+    animalId: 'racoon',
+    name: 'racoon',
     difficulty: 2,
   },
-  shark: {
-    animalId: 'shark',
-    name: 'shark',
-    difficulty: 2,
-  },
+  // rooster: {
+  //   animalId: 'rooster',
+  //   name: 'rooster',
+  //   difficulty: 2,
+  // },
+  // shark: {
+  //   animalId: 'shark',
+  //   name: 'shark',
+  //   difficulty: 2,
+  // },
   sheep: {
     animalId: 'sheep',
     name: 'sheep',
@@ -149,19 +189,24 @@ const animalsData = {
   //   name: 'sheep',
   //   difficulty: 2,
   // },
-  snail: {
-    animalId: 'snail',
-    name: 'snail',
-    difficulty: 2,
-  },
-  squid: {
-    animalId: 'squid',
-    name: 'squid',
-    difficulty: 2,
-  },
+  // snail: {
+  //   animalId: 'snail',
+  //   name: 'snail',
+  //   difficulty: 2,
+  // },
+  // squid: {
+  //   animalId: 'squid',
+  //   name: 'squid',
+  //   difficulty: 2,
+  // },
   squirrel: {
     animalId: 'squirrel',
     name: 'squirrel',
+    difficulty: 2,
+  },
+  tiger: {
+    animalId: 'tiger',
+    name: 'tiger',
     difficulty: 2,
   },
   // unknown01: {
@@ -184,9 +229,19 @@ const animalsData = {
   //   name: 'unknown04',
   //   difficulty: 2,
   // },
+  walrus: {
+    animalId: 'walrus',
+    name: 'walrus',
+    difficulty: 1,
+  },
   wolf: {
     animalId: 'wolf',
     name: 'wolf',
+    difficulty: 1,
+  },
+  zebra: {
+    animalId: 'zebra',
+    name: 'zebra',
     difficulty: 1,
   },
 };
