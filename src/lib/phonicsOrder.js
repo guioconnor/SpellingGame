962ba3f1488977acdbefa552fe277b48fd83c1f9
Guilherme O'Connor <guilherme@fundapps.co>
2017-11-11 23:27:56 +0000

    Phonics order (for future use)

---
+++ src/lib/phonicsOrder.js
@@ -0,0 +1,10 @@
+export default order = [
+  ['m', 'a', 's', 'd', 't'],
+  ['i', 'n', 'p', 'g', 'o'],
+  ['c', 'k', 'u', 'b'],
+  ['f', 'e', 'l', 'h', 'sh', 'r'],
+  ['j', 'v', 'y', 'w'],
+  ['th', 'z', 'ch', 'qu', 'x', 'ng', 'nk'],
+  ['ay', 'ee', 'igh', 'ow', 'oo', 'oo', 'ar', 'or', 'air', 'ir', 'ou', 'oy'],
+  ['ea', 'oi', 'a.?e', 'i.?e', 'o.?e', 'u.?e', 'aw', 'are', 'ur', 'er', 'ow', 'ai', 'oa', 'ew', 'ire', 'ear', 'ure'],
+]
